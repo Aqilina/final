4710aa73e1c2769e2aeefeed4b80b62a5189c42a
akvilina <akvilina.lapenaite@gmail.com>
2021-03-01 22:23:11 +0200

    Validation used in AuthController, checked request type and validation

--- core/AuthController.php
+++ core/AuthController.php
@@ -4,15 +4,87 @@
 namespace app\core;
 
 
+use app\core\Controller;
+
 
 
 class AuthController extends Controller
+
 {
-    public static function login()
+    public Validation $vld;
+
+    public function __construct()
+    {
+        $this->vld = new Validation;
+    }
+
+
+    public function register(Request $request)
+    {
+        if ($request->isGet()) :
+
+            $data = [
+                'name' => '',
+                'lastname' => '',
+                'email' => '',
+                'phone' => '',
+                'address' => '',
+                'password' => '',
+                'confirmPassword' => '',
+                'errors' => [
+                    'nameErr' => '',
+                    'lastname' => '',
+                    'emailErr' => '',
+                    'phoneErr' => '',
+                    'addressErr' => '',
+                    'passwordErr' => '',
+                    'confirmPasswordErr' => '',
+                ],
+            ];
+//            var_dump($data);
+            return $this->render('register', $data);
+        endif;
+
+        if ($request->isPost()) :
+            $data = $request->getBody();
+
+            $data['errors']['nameErr'] = $this->vld->validateName($data['name']);
+            $data['errors']['nameErr'] = $this->vld->validateLastname($data['lastname']);
+            $data['errors']['emailErr'] = $this->vld->validateEmail($data['email']);
+//            $data['errors']['emailErr'] = $this->vld->validateEmail($data['email']);
+            $data['errors']['passwordErr'] = $this->vld->validatePassword($data['password'], 6, 10);
+            $data['errors']['confirmPasswordErr'] = $this->vld->confirmPassword($data['confirmPassword']);
+
+            //JEI NERA KLAIDU
+            if ($this->vld->ifEmptyArr($data['errors'])) :
+
+                //uzhashinti slaptazodi
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                if ($this->userModel->register($data)) {
+//                    $request->redirect('/login');
+                } else {
+                    die('Something went wrong in adding user to db');
+                }
+
+            endif;
+
+            var_dump($data);
+            return $this->render('register', $data);
+        endif;
+
+    }
+
+
+
+//    --------------------------------------------------------------------
+    public function login()
     {
         $data = [
             'vardas' => 'Aqilina'
         ];
-        return Application::$app->router->renderView('login', $data);
+        return $this->render('login', $data);
     }
+
+
 }
