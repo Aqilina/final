7bdf81281e513c8962a4aa8f6010b5052884cb2c
akvilina <akvilina.lapenaite@gmail.com>
2021-03-01 22:20:10 +0200

    index is an array, added register.php

--- public/index.php
+++ public/index.php
@@ -4,13 +4,17 @@ require_once '../vendor/autoload.php';
 
 use app\core\Application;
 use app\core\AuthController;
+use app\controller\SiteController;
 
 $app = new Application(dirname(__DIR__));
 
 
-$app->router->get('/', 'index');
-$app->router->get('/index', 'index');
+$app->router->get('/', [SiteController::class, 'index']);
+$app->router->get('/index', [SiteController::class, 'index']);
 
 $app->router->get('/login', [AuthController::class, 'login']);
+
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
