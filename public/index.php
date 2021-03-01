e52ff95273475455021e0cc60fa384e64a77fbc8
akvilina <akvilina.lapenaite@gmail.com>
2021-03-01 22:52:25 +0200

    created $config using $_ENV, whic was created with composer vlucas/dotenv.

--- public/index.php
+++ public/index.php
@@ -6,7 +6,20 @@ use app\core\Application;
 use app\core\AuthController;
 use app\controller\SiteController;
 
-$app = new Application(dirname(__DIR__));
+
+//COMPOSER.JSON inicijuota. kad uzkrauti kintamuosius
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
 
 
 $app->router->get('/', [SiteController::class, 'index']);
